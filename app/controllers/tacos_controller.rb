660957cccb55cdf358f7969f408865fb4e2c92e0
tatertotcode <elijah.kravets@kellogg.northwestern.edu>
2026-02-10 17:26:14 +0000

    lecture activities and built out dice game

---
+++ app/controllers/tacos_controller.rb
@@ -0,0 +1,6 @@
+class TacosController < ApplicationController
+  def index
+    # rendering views/tacos/index.html.erb
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
+  get("/tacos", :controller => "tacos", :action => "index")
+  get("/dice", :controller => "dice", :action => "index")
+  get("/companies", :controller => "companies", :action => "index")
+
 end
